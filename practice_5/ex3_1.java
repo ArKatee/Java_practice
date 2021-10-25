8ebecae9f7f6bfc495f1bbaf24f0e0aefe442808
ArKatee <ArKate02@yandex.ru>
2021-10-25 12:57:28 +0300

    practice_5

---
+++ practice_5/ex3_1.java
@@ -0,0 +1,53 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class ex3_1 {
+    public static void main(String[] args) {
+        Deque<Integer> player1 = new LinkedList<Integer>();
+        Deque<Integer> player2 = new LinkedList<Integer>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            player1.add(scanner.nextInt());
+        }
+        for (int i = 0; i < 5; i++) {
+            player2.add(scanner.nextInt());
+        }
+
+
+        for (int i = 1; i <= 106; i++) {
+            Integer a, b;
+            a = player1.poll();
+            b = player2.poll();
+            if (a == 0 && b == 9) {
+                player1.add(a);
+                player1.add(b);
+            } else if (a == 9 && b == 0) {
+                player2.add(a);
+                player2.add(b);
+            } else if (a > b) {
+                player1.add(a);
+                player1.add(b);
+            } else if (b > a) {
+                player2.add(a);
+                player2.add(b);
+            }
+
+            if (player1.isEmpty()) {
+                System.out.print("second ");
+                System.out.print(i);
+                return;
+            }
+            if (player2.isEmpty()) {
+                System.out.print("first ");
+                System.out.print(i);
+                return;
+            }
+        }
+
+        System.out.print("botva");
+    }
+}
